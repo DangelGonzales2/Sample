2d6974a88a2d0cdc2a2a3e38e78c22301e1a9608
DangelGonzales2 <daniel3332014@gmail.com>
2016-09-03 21:34:16 -0300

    Add files via upload

---
+++ Albun_Musica/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+
+
+    'Declaro una variable para saber el estado del Formulario
+    Dim state As Integer
+    Dim px, py As Integer
+    Dim mover As Boolean
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+
+
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        px = e.X
+        py = e.Y
+        mover = True
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        'Cuando el mose se esta moviendo y se esta arastrando el form
+        If mover Then
+            Me.Location = Me.PointToScreen(New Point(Me.MousePosition.X - Me.Location.X - px, Me.MousePosition.Y - Me.Location.Y - py))
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
+        'Cuando el mose se aleja del picture bos
+        mover = False
+    End Sub
+End Class
